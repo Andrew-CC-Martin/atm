2afd0aad1a8cac0eb10103badd112984ce3e9cf3
Andrew-CC-Martin <amartin262@gmail.com>
2020-12-04 12:11:01 +1100

    add invalid input handling. add program exit

--- index.rb
+++ index.rb
@@ -3,3 +3,35 @@
 def hello
   return "hello world"
 end
+
+def welcome_and_input
+  system("clear")
+  puts "Welcome to your friendly neighbourhood ATM!"
+  puts "Please select from the following options:"
+  puts "1. Display balance"
+  puts "2. Make a withdrawl"
+  puts "3. Make a deposit"
+  puts "4. Exit"
+  input = gets.chomp.to_i
+  case input
+  when 1
+    # show balance
+  when 2
+    # make a withdrawl
+  when 3
+    # make a deposit
+  when 4
+    # exit the program
+    puts "goodbye"
+    system("exit")
+  else
+    # print error message for invalid input
+    # and re-print welcome
+    puts "Invalid input, please enter a number from 1-4"
+    puts "Press any key to continue"
+    gets
+    welcome_and_input()
+  end
+end
+
+welcome_and_input()
